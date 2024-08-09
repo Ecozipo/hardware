43ae870e3cc5439ee39d61d9840abfed66f0bcc6
ValicioKely <razalicio008@gmail.com>
2024-08-09 06:40:40 +0300

    Ajout du code pour le PMZE

--- code/pio/src/main.cpp
+++ code/pio/src/main.cpp
@@ -1,42 +1,39 @@
 #include<Arduino.h>
 #include "config.h"
-
 #include <WiFiClientSecure.h>
-#include <MQTTClient.h> //MQTT Library Source: https://github.com/256dpi/arduino-mqtt
-
-#include <ArduinoJson.h> //ArduinoJson Library Source: https://github.com/bblanchon/ArduinoJson
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
 #include "WiFi.h"
-
-// MQTT topics for the device
-#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
-#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
-
-#define LED_BUILTIN 2
-
+#include <PZEM004Tv30.h>
 /*
  *
- *  Local variables
+ *  Global variables
  *
  */
-float voltage; // Variable to store voltage value
+#define AWS_IOT_PUBLISH_TOPIC   "esp32/pzem"
+#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/led"
+#define LED_BUILTIN 2
+
+PZEM004Tv30 pzem(Serial2, 16, 17);
+float voltage;
+float current;
+float power;
+float energy;
 float freq;
+float pf;
 
 
 WiFiClientSecure wifi_client = WiFiClientSecure();
-MQTTClient mqtt_client = MQTTClient(256); //256 indicates the maximum size for packets being published and received.
+MQTTClient mqtt_client = MQTTClient(256);
 
 void incomingMessageHandler(String& topic, String& payload);
 uint32_t t1;
 
 void connectAWS()
 {
-    //Begin WiFi in station mode
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
     Serial.println("Connecting to Wi-Fi");
-
-    //Wait for WiFi connection
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
@@ -44,52 +41,43 @@ void connectAWS()
     }
     Serial.println();
 
-    // Configure wifi_client with the correct certificates and keys
     wifi_client.setCACert(AWS_CERT_CA);
     wifi_client.setCertificate(AWS_CERT_CRT);
     wifi_client.setPrivateKey(AWS_CERT_PRIVATE);
 
-    //Connect to AWS IOT Broker. 8883 is the port used for MQTT
     mqtt_client.begin(AWS_IOT_ENDPOINT, 8883, wifi_client);
-
-    //Set action to be taken on incoming messages
     mqtt_client.onMessage(incomingMessageHandler);
 
     Serial.print("Connecting to AWS IOT");
-
-    //Wait for connection to AWS IoT
     while (!mqtt_client.connect(THINGNAME))
     {
         Serial.print(".");
         delay(100);
     }
     Serial.println();
-
     if (!mqtt_client.connected())
     {
         Serial.println("AWS IoT Timeout!");
         return;
     }
-
-    //Subscribe to a topic
     mqtt_client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
-
     Serial.println("AWS IoT Connected!");
 }
 
 void publishMessage()
 {
-    //Create a JSON document of size 200 bytes, and populate it
-    //See https://arduinojson.org/
-    StaticJsonDocument < 200 > doc;
+    StaticJsonDocument<200> doc;
     doc["id"] = random(1000);
     doc["created_at"] = millis() - t1;
     doc["voltage"] = voltage;
-    doc["Freq"] = freq;
-    char jsonBuffer[512];
-    serializeJson(doc, jsonBuffer); // print to mqtt_client
+    doc["current"] = current;
+    doc["energy"] = energy;
+    doc["freq"] = freq;
+    doc["pf"] = pf;
 
-    //Publish to the topic
+    char jsonBuffer[512];
+    serializeJson(doc, jsonBuffer);
+
     mqtt_client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
     Serial.println("message sent !");
 }
@@ -100,11 +88,9 @@ void incomingMessageHandler(String& topic, String& payload)
     Serial.println("Topic: " + topic);
     Serial.println("Payload: " + payload);
 
-    // Parse the JSON payload
-    StaticJsonDocument < 200 > doc; // Adjust size according to your needs
+    StaticJsonDocument<200> doc;
     DeserializationError error = deserializeJson(doc, payload);
 
-    // Check if parsing succeeds
     if (error)
     {
         Serial.print(F("deserializeJson() failed: "));
@@ -112,10 +98,8 @@ void incomingMessageHandler(String& topic, String& payload)
         return;
     }
 
-    // Extract the value of "message"
-    const char* message = doc["message"]; // "ON" or "OFF"
+    const char* message = doc["message"];
 
-    // Compare the message and act accordingly
     if (strcmp(message, "ON") == 0)
     {
         digitalWrite(LED_BUILTIN, HIGH);
@@ -138,6 +122,13 @@ void setup()
 
 void loop()
 {
+    voltage = pzem.voltage();
+    current = pzem.current();
+    power = pzem.power();
+    energy = pzem.energy();
+    freq = pzem.frequency();
+    pf = pzem.pf();
+
     publishMessage();
     mqtt_client.loop();
     delay(4000);
